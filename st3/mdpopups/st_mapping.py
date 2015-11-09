c16780c058de704ec0ddf29444d4008ea31fc893
facelessuser <faceless.shop@gmail.com>
2015-11-09 01:06:17 -0700

    More language mappings

--- st3/mdpopups/st_mapping.py
+++ st3/mdpopups/st_mapping.py
@@ -1,16 +1,30 @@
 """Sublime Text language mapping."""
 
 lang_map = {
-    "actionscript": (("actionscript",), ('ActionScript/ActionScript',)),
+    "actionscript": (("as", "actionscript",), ('ActionScript/ActionScript',)),
     "applescript": (("applescript",), ('AppleScript/AppleScript',)),
     "asp": (("asp",), ('ASP/ASP',)),
+    "bash": (('bash', 'sh', 'ksh', 'shell'), ('ShellScript/Shell-Unix-Generic',)),
     "batch": (("batch", "bat", "dosbatch", "winbatch"), ('Batch File/Batch File',)),
     "c": (("c",), ('C++/C')),
-    "c++": (("c++", "cpp"), ('C++/C++')),
+    "c++": (("c++", "cpp"), ('C++/C++',)),
+    "c#": (('csharp', 'c#'), ("C#/C#",)),
+    "clojure": (("clojure", "clj"), ("Clojure/Clojure",)),
     "css": (("css",), ('CSS/CSS',)),
     "diff": (("diff",), ('Diff/Diff',)),
+    "go": (("go",), ('Go/Go',)),
+    "groovy": (("groovy",), ("Groovy/Groovy",)),
     "html": (("html",), ('HTML/HTML',)),
     "javascript": (("javascript", "js"), ("JavaScript/JavaScript", "JavaScriptNext - ES6 Syntax/JavaScriptNext")),
+    "markdown": (("markdown", "md"), ("Markdown/Markdown",)),
+    "makefile": (("makefile", "make", "mf", "bsdmake"), ("Makefile/Makefile",)),
+    "matlab": (("matlab",), ("Matlab/Matlab",)),
+    "perl": (("perl", 'pl'), ("Perl/Perl", "ModernPerl/ModernPerl")),
+    "php": (('php', 'php3', 'php4', 'php5'), ("PHP/PHP",)),
     "python": (("python", "py"), ('Python/Python', 'MagicPython/grammars/MagicPython')),
-    "text": (("text",), ('Text/Plain text',))
+    "regex": (("regex", "regexp"), ("Regular Expressions/RegExp",)),
+    "ruby": (("rb", "ruby"), ('Ruby/Ruby',)),
+    "text": (("text",), ('Text/Plain text',)),
+    "xml": (("xml",), ('XML/XML',)),
+    "yaml": (("yaml",), ('YAML/YAML',))
 }
