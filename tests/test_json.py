c2e573f96cf9b0b2e374d5c3953c7770e61d2788
facelessuser <faceless.shop@gmail.com>
2021-04-22 21:15:05 -0600

    Fix tests

--- tests/test_json.py
+++ tests/test_json.py
@@ -14,9 +14,7 @@ class TestSettings(unittest.TestCase):
         for root, dirnames, filenames in os.walk(folder):
             for filename in fnmatch.filter(filenames, pattern):
                 yield os.path.join(root, filename)
-            for dirname in [d for d in dirnames if d not in ('.svn', '.git', '.tox')]:
-                for f in self._get_json_files(pattern, os.path.join(root, dirname)):
-                    yield f
+            dirnames = [d for d in dirnames if d not in ('.svn', '.git', '.tox')]
 
     def test_json_settings(self):
         """Test each JSON file."""
@@ -32,7 +30,6 @@ class TestSettings(unittest.TestCase):
 
         for pattern in patterns:
             for f in self._get_json_files(pattern):
-                print(f)
                 self.assertFalse(
                     validate_json_format.CheckJsonFormat(False, True).check_format(f),
                     "%s does not comform to expected format!" % f
